7e321644e8da60aa79f80ee9ce1a16ea0203d719
heywei <heywei@gmail.com>
2015-09-05 01:29:17 +0100

    add the model and DAO interface

---
+++ src/main/java/com/pm/core/dao/GroupDao.java
@@ -0,0 +1,9 @@
+package com.pm.core.dao;
+
+import com.pm.core.model.Group;
+import com.pm.core.orm.GeneralDAO;
+
+public interface GroupDao extends GeneralDAO<Group, Long>
+{
+	Group findByName(String name);
+}
